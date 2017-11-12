d666a226f23fa7bd7c7ded7212cad027809b3c24
abdelrahman elsharawy <abdoalsharawy@gmail.com>
2017-11-12 23:21:06 +0200

    Update production_settings.py

--- TodoList/settings/production_settings.py
+++ TodoList/settings/production_settings.py
@@ -4,8 +4,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'test.todoListApi@gmail.com'
-EMAIL_HOST_PASSWORD = 'test987654'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
 EMAIL_PORT = 587
 
 
